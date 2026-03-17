de4623e19c10d74ca4dd17c07606a0e075c52566
Lang Cheng <lang.cheng@student.manchester.ac.uk>
2026-03-17 22:48:10 +0000

    delete

--- leo_exploration_ws/build/leo_exploration/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/group4/LeoRoverAutonomousExploration/leo_exploration_ws/install/leo_exploration'
